1a1a1d19606b99b3bc33f77871a753f480eb3514
selfup <boudinot.regis@yahoo.com>
2016-01-09 17:13:22 -0700

    web app api

---
+++ web/router.ex
@@ -0,0 +1,37 @@
+defmodule MarketApi.Router do
+  use MarketApi.Web, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
+  pipeline :api do
+    plug :accepts, ["json-api"]
+    plug JaSerializer.ContentTypeNegotiation
+    plug JaSerializer.Deserializer
+  end
+
+  scope "/", MarketApi do
+    pipe_through :browser # Use the default browser stack
+
+    get "/", PageController, :index
+  end
+
+  scope "/api", MarketApi do
+  pipe_through :api
+
+  resources "/markets", MarketController, except: [:new, :edit] do
+    resources "/products", ProductController, only: [:index]
+  end
+
+    resources "/products", ProductController, except: [:new, :edit]
+  end
+# Other scopes may use custom stacks.
+  # scope "/api", MarketApi do
+  #   pipe_through :api
+  # end
+end
